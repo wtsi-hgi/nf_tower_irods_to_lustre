267cd345d9f0864a9e9f8a6e9d6e92b20abe62c2
Guillaume G Noell <gn5@sanger.ac.uk>
2021-05-11 14:49:27 +0100

    auto-git

--- bin/google_spreadsheet_to_csv.py
+++ bin/google_spreadsheet_to_csv.py
@@ -8,18 +8,21 @@ from logdecorator import log_on_start, log_on_end, log_on_error, log_exception
 @click.command()
 @click.option('--creds_json', required=True, type=click.Path(exists=True), help='Google API credentials')
 @click.option('--gsheet', required=True, help='name of input Google spreadsheet')
+@click.option('--sheet', required=False, default='sheet1', show_default=True,
+              help='optional sheet name if spreadsheet has multiple sheets')
 @click.option('--output_csv_name', required=True, help='path or name of output csv file')
 
 @log_on_start(logging.INFO, (
     "\ncreds_json: {creds_json:s}\n"
     + "gsheet: {gsheet:s}\n"
+    + "sheet: {sheet:s}\n"
     + "output_csv_name: {output_csv_name:s}"))
-def gsheet_to_csv(creds_json, gsheet, output_csv_name):
+def gsheet_to_csv(creds_json, gsheet, sheet, output_csv_name):
     """Use google API and Service Account to convert google Spreadsheet to local csv file."""
     creds = ServiceAccountCredentials.from_json_keyfile_name(creds_json)
     client = gspread.authorize(creds)
-    sheet = client.open(gsheet).sheet1
-    data = sheet.get_all_values()
+    google_sheet = client.open(gsheet).worksheet(sheet)
+    data = google_sheet.get_all_values()
     headers = data.pop(0)
     df = pd.DataFrame(data, columns=headers)
     df = df.stack().str.replace(',',';').unstack()
